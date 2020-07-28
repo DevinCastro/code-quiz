d4992e858b382bf1a890dc332364e13e41bec735
Devin Castro <dcrocker21@gmail.com>
2020-07-27 20:34:36 -0700

    Added code comments

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
-// first lets populate a question and some answers
-
+// First these are the questions for my quiz that ive made into an object
 let questions = {
   question1: '1. What is the HTML tag under which one can write the JavaScript code?',
   question2: '2. Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?',
   question3: '3. JavaScript is a ___ -side programming language.',
 }
 
-
+// next I have my possible answers for the questions that are an object of objects
 let answers = {
   answer1: {
     a: 'javascript',
@@ -28,13 +27,16 @@ let answers = {
   },
 }
 
+// start an empty array to hold all of the users choices.
 userChoices = []
+// store this in the local storage
 localStorage.setItem('userChoices', JSON.stringify(userChoices))
-// delete this later
 
-
+// start with the users score at 0
 let score = 0
+// this count will be the number of seconds the user has for the quiz
 let count = 60
+// bring this variable into existence
 let myInterval
 
 // =============================================
@@ -48,38 +50,37 @@ document.getElementById('start').addEventListener('click', event => {
   // this will run the function every interval on miliseconds
   // interval to decrement amount of time by each one second
   myInterval = setInterval(() => {
-    console.log(count)
     // displays time left on screen
+    // moves count down by 1
     count--
+    // Show the number of seconds left on the screen
     document.getElementById('count').textContent = `
     ${count} Seconds
     `
-    // moves count down by 1
+
+    // if at any point the time runs out, then execute this logic:
     if (count <= 0) {
 
-      console.log('works')
+      // clear the interval
       clearInterval(myInterval)
 
-
-
+      // clear out the count element
       document.getElementById('count').innerHTML = ``
-
+      // Show the all finished message
       document.getElementById('question').innerHTML = `
       All Finished!
       `
+      // display the final score
       document.getElementById('answers').innerHTML = `
       Great job! Your score is: ${score}
       `
-
+      // Execute our displayHighScores function
       displayHisghscores()
 
     }
   }, 1000)
 
-
-  document.getElementById('count').textContent = `
-    ${count} Seconds
-    `
+  // clear out the intro element
   document.getElementById('intro').innerHTML = ``
 
   // fill the question element with the following HTML
@@ -108,18 +109,21 @@ document.addEventListener('click', event => {
     // decrement the time left by 10 seconds if there is a wrong answer
     count = count - 10
 
+    // give the wrong answer element the data attribute "wrong"
     event.target.dataset.wrong = 'wrong'
+    // push the strong "wrong" into our userChoices array
     userChoices.push(event.target.dataset.wrong)
+    // update the local stroage
     localStorage.setItem('userChoices', JSON.stringify(userChoices))
 
 
-    // reset the answers element
+    // reset the answers element with a "wrong" message and a button to move on to te next question
     document.getElementById('answers').innerHTML = `
     WRONG
     <button id="next">Next</button>   
     `
 
-    // start the next question
+    // listen for when the user clicks the next button and then start the next question!
     document.getElementById('next').addEventListener('click', event => {
       document.getElementById('question').innerHTML = `
         ${questions.question2}
@@ -131,17 +135,14 @@ document.addEventListener('click', event => {
         <button class="rightChoice1">D: ${answers.answer2.d}</button>
         `
     })
-  } else if (event.target.classList.contains('rightChoice')) {
-    console.log('right')
-    // reset the answers element
-
-
+  } else if (event.target.classList.contains('rightChoice')) { //All this logic is exactly the same except this is the case they pick the correct answer
+    
     event.target.dataset.correct = 'correct'
     userChoices.push(event.target.dataset.correct)
     localStorage.setItem('userChoices', JSON.stringify(userChoices))
 
 
-
+    // display a CORRECT message!
     document.getElementById('answers').innerHTML = `
     CORRECT!
     <button id="next">Next</button> 
@@ -164,11 +165,10 @@ document.addEventListener('click', event => {
 
 // =============================================
 
-// now set an event listener to the entire page
+// Restart this entire process but we are on the second question and then will be going through to the third question
 document.addEventListener('click', event => {
   // This logic is for the case when we click a list element with the class 'wrongChoice' and we want to execute the following
   if (event.target.classList.contains('wrongChoice1')) {
-    console.log('wrong1')
 
     count = count - 10
 
@@ -195,8 +195,7 @@ document.addEventListener('click', event => {
         `
     })
   } else if (event.target.classList.contains('rightChoice1')) {
-    console.log('right1')
-    // reset the answers element
+
     document.getElementById('answers').innerHTML = `
     CORRECT!
     <button id="next1">Next</button> 
@@ -226,11 +225,10 @@ document.addEventListener('click', event => {
 
 // =============================================
 
-
+// after the final question we excute the same logic one more time!
 document.addEventListener('click', event => {
 
   if (event.target.classList.contains('wrongChoice2')) {
-    console.log('wrong2')
 
     event.target.dataset.wrong = 'wrong'
     userChoices.push(event.target.dataset.wrong)
@@ -238,8 +236,6 @@ document.addEventListener('click', event => {
 
 
   } else if (event.target.classList.contains('rightChoice2')) {
-    console.log('right2')
-    // reset the answers element
 
     event.target.dataset.correct = 'correct'
     userChoices.push(event.target.dataset.correct)
@@ -249,10 +245,12 @@ document.addEventListener('click', event => {
 
 
   // This will finish the quiz 
+  // we are already using a global event listener, so regardless, whatever the user choses will execture the following logic
   if (event.target.classList.contains('wrongChoice2') || event.target.classList.contains('rightChoice2')) {
 
-    for (i = 0; i < userChoices.length; i++) {
 
+    // create a loop to go through our userChoices array and find out how many correct answers they got
+    for (i = 0; i < userChoices.length; i++) {
 
       // gives the user 100 points per correct answer
       if (userChoices[i] === 'correct') {
@@ -261,13 +259,12 @@ document.addEventListener('click', event => {
 
     }
 
+    // set the time to zero so we clear out the interval
     count = 0
-    // if we are out of time we run this
 
     // This logic is for the case when we click a list element with the class 'wrongChoice' and we want to execute the following
     if (count <= 0) {
 
-      console.log('works')
       clearInterval(myInterval)
 
 
@@ -294,7 +291,7 @@ document.addEventListener('click', event => {
 // ===========================================
 
 
-// Listen for someone to click the viewScores button and the display JUST the highscores
+// At any time Listen for someone to click the viewScores button and the display JUST the highscores
 document.getElementById('viewScores').addEventListener('click', event => {
 
   // execute our displayHighScores function
@@ -307,8 +304,6 @@ document.getElementById('viewScores').addEventListener('click', event => {
 
 
 })
-
-
 
 
 // ===========================================
@@ -338,7 +333,6 @@ function displayHisghscores() {
 
   // set highScoreNames to whatever is in local storage OR an empty array
   let highScoreNames = JSON.parse(localStorage.getItem('highScoreNames')) || []
-  console.log(highScoreNames)
 
 
   // this starts the loop that will go through and input list elements for each of the PREVIOUS scores in the local storage
@@ -361,7 +355,6 @@ function displayHisghscores() {
   // listen for when the button is clicked and then do this function
   document.getElementById('addScore').addEventListener('click', event => {
     event.preventDefault()
-    console.log('hello')
 
     let highScoreNamesObj = {
       name: document.getElementById('initials').value,
